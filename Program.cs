b25347a843889f84f6c0a4e395a255ea629157b9
Avinash Chowdary Bodduluri <avinash.bodduluri@gmail.com>
2024-09-27 11:52:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AvinashChowdaryBodduluri_Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
