25439be45be57d413ed8b06647dc980dea3d8787
Avinash Chowdary Bodduluri <avinash.bodduluri@gmail.com>
2024-09-27 13:45:47 +0200

    Added Car Implementation

--- Models/CarImpl.cs
+++ Models/CarImpl.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,54 @@ namespace AvinashChowdaryBodduluri_Assignment2.Models
 {
     internal class CarImpl : ICar
     {
+        public int Doors { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Mileage { get; set; }
+
+        private bool engineOn = false;
+
+        public CarImpl(string brand, string model, int year, double mileage, int doors) 
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Year = year;
+            this.Mileage = mileage;
+            this.Doors = doors;
+        }
+
+        public string Drive()
+        {
+            if(engineOn)
+            {
+                return "Car is Driveable & Ready for ride.";
+            }
+            else
+            {
+                return "Engine is off, Start the engine first.";
+            }
+        }
+
+        public bool IsEngineOn()
+        {
+            return engineOn;
+        }
+
+        public void StartEngine()
+        {
+            engineOn = true;
+        }
+
+        public void StopEngine()
+        {
+            engineOn = false;
+
+        }
+        public override string ToString()
+        {
+            return $"Car: {Brand} {Model} ({Year}) with {Mileage} km on the clock and had {Doors} doors.";
+        }
+
     }
 }
