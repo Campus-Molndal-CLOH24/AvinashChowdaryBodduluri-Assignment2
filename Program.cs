72ac09bf7c3aa3df1d241556a9839a3b4f9a9ed1
Avinash Chowdary Bodduluri <avinash.bodduluri@gmail.com>
2024-09-29 16:16:52 +0200

    Added Cars & Motorcycle data to vehicle list

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
     using AvinashChowdaryBodduluri_Assignment2.Interfaces;
     using AvinashChowdaryBodduluri_Assignment2.Factories;
     using AvinashChowdaryBodduluri_Assignment2.Models;
+    using System.Security.Cryptography.X509Certificates;
+
     internal class Program
     {
         static void Main()
@@ -15,22 +17,24 @@
 
             vehicles.Add(carFactory.CreateCar("Toyota", "Corolla", 2020, 15000, 4));
             vehicles.Add(motorcycleFactory.CreateMotorcycle("Harley Davidson", "Sportster", 2019, 5000, "V-Twin"));
+            vehicles.Add(carFactory.CreateCar( "Honda", "Civic", 2021, 20000, 4));
+            vehicles.Add(motorcycleFactory.CreateMotorcycle("Yamaha", "YZF-R1", 2020, 10000, "Inline-4"));
+            vehicles.Add(carFactory.CreateCar("Ford", "Fiesta", 2018, 30000, 4));   
+            vehicles.Add(motorcycleFactory.CreateMotorcycle("Kawasaki", "Ninja", 2017, 15000, "Parallel Twin"));
 
-            foreach(IVehicle vehicle in vehicles)
+            foreach (IVehicle vehicle in vehicles)
             {
+                Console.WriteLine("===============================================");
                 Console.WriteLine(vehicle);
                 if(vehicle.IsEngineOn()==false)
                 {
                     vehicle.StartEngine();
                 }
-                //Console.WriteLine("Vehicle engine status: " + (vehicle.IsEngineOn() ? "On" : "Off"));
-                //vehicle.StartEngine();
                 if(vehicle is IDriveable driveable)
                 {
                     Console.WriteLine(driveable.Drive());
                 }
                 vehicle.StopEngine();
-                //Console.WriteLine("Vehicle engine status: " + (vehicle.IsEngineOn() ? "On" : "Off"));
             }
 
             Console.ReadKey();
